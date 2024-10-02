4def16bd2a065f95ca9220101cc079d6d35c477e
Saideepak9676 <gsaideepak.sd@gmail.com>
2024-10-01 23:36:26 -0400

    added the file

---
+++ calculator/calculation.py
@@ -0,0 +1,21 @@
+# calculator/calculation.py
+from decimal import Decimal
+from typing import Callable
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculation:
+    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        self.a = a
+        self.b = b
+        self.operation = operation
+    
+    def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        return Calculation(a, b, operation)
+
+    def perform(self):
+        if self.operation == divide and self.b == Decimal('0'):
+            raise ValueError("Cannot divide by zero")
+        return self.operation(self.a, self.b)
+
+    def __repr__(self):
+        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
